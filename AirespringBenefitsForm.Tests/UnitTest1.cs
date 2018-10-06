316b007d5c43f7e8b0571858a1905b449b128c98
Xellarant <xellarant@gmail.com>
2018-10-06 05:43:36 -0700

    Add project files.

---
+++ AirespringBenefitsForm.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AirespringBenefitsForm.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
